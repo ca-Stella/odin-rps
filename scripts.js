a9b5392a4147ff172e365459da4c06904e1083b3
ca-Stella <stella.2532@gmail.com>
2022-07-01 14:26:26 -0600

    Add more HTML structures and Clean code for better readability

--- scripts.js
+++ scripts.js
@@ -2,16 +2,29 @@
 const choices = ['Rock', 'Paper', 'Scissors'];
 let results = 0;
 let pScore = cScore = 0;
-let cSelection = 'none';
+let cSelection = pSelection = 'none';
 
 // buttons
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.querySelector('#scissors');
 
+// result elements
+const tallyResults = document.querySelector('.tally');
+const roundResults = document.querySelector('.round-results');
+
 // playRound for each button press
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', playRound));
+
+// playRound() function to play a round of RPS
+function playRound(pSelection) {
+    pSelection = this.textContent;
+    cSelection = computerPlay(choices);
+    let result = checkWinner(pSelection, cSelection);
+    declareResults(result);
+    tallyUp();
+}
 
 // computerPlay function to randomly select
 function computerPlay(array) {
@@ -19,15 +32,11 @@ function computerPlay(array) {
     return array[randomNumber];
 }
 
-// playRound() function to play a round of RPS
-function playRound(pSelection, cSelection) {
-    pSelection = this.textContent;
-    let cSelection = computerPlay(choices);
-    
-    if (pSelection !== 'Rock' && pSelection !== 'Paper' && pSelection !== 'Scissors') {
-        results = 4;
-    } else if (pSelection === cSelection) {
-        results = 2;
+// checkWinner() helper function
+function checkWinner(pSelection, cSelection) {
+    let result = 0;
+    if (pSelection === cSelection) {
+        result = 2;
     } else {
         if (cSelection === 'Rock') {
             results = (pSelection === 'Paper') ?  1 : 3 ;
@@ -37,20 +46,31 @@ function playRound(pSelection, cSelection) {
             results = (pSelection === 'Paper') ?  3 : 1 ;
         }
     }
+    return result;
+}
 
+// tallyUp() function to add scores
+function tallyUp() {
+    tallyResults.textContent = (pScore + '+' + cScore);
+}
+
+//declareResults() function to post results
+function declareResults(result) {
+    const sent = document.createElement('div');
     switch (result) {
         case 2: 
-            console.log("It's a tie!");
+            sent.textContent = "It's a tie!";
             break;
         case 1: 
-            console.log("You win! " + pSelection + " beats " + cSelection + "!");
             pScore++;
+            sent.textContent = ("You win! " + pSelection + " beats " + cSelection + "!");
             break;
         case 3: 
-            console.log("You lose! " + cSelection + " beats " + pSelection + "!");
             cScore++;
+            sent.textContent = ("You lose! " + cSelection + " beats " + pSelection + "!");
             break;
         default: 
-            alert("please input one of Rock, Paper, or Scissors!");
+            sent.textContent = ("Something's Wrong!");
     }
+    roundResults.insertBefore(sent, roundResults.firstChild);
 }
